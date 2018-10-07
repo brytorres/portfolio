ddc4540d36967bd7dd5758f44cb10bbe5d79250f
brytorres <bryan@torvel.us>
2018-10-07 19:09:49 -0400

    Clean up

--- src/components/ProjectsSection.js
+++ src/components/ProjectsSection.js
@@ -1,28 +1,10 @@
 import React, { Component } from 'react'
 import Project from '../components/Project'
-// import config from '../config/config';
 
 import styles from '../styles/projects.module.scss';
 
 class ProjectsSection extends Component {
   render() {
-
-    const technologies = [
-      [
-        'HTML/CSS',
-        'Javascript'
-      ],
-      [
-        'HTML/CSS',
-        'Javascript',
-        'PHP (Laravel)'
-      ],
-      [
-        'HTML/CSS',
-        'Javascript',
-        'Python'
-      ]
-    ]
 
     const clientProjects = this.props.clientProjects
 
@@ -69,33 +51,6 @@ class ProjectsSection extends Component {
               />
             })
           }
-
-          {/* <Project
-            client='Retrograde Zone'
-            projectUrl='https://www.torvel.us'
-            description='I did this, that and the other'
-            version='v2.1'
-            status='broken'
-            technologies={technologies[0]}
-          />
-
-          <Project
-            client='Mi Casa'
-            projectUrl='https://www.eatatmiscasa.com'
-            description='I did this, that and the other'
-            version='v2.0'
-            status='completed'
-            technologies={technologies[1]}
-          />
-
-          <Project
-            client='Pro192 Studios'
-            projectUrl='https://www.pro192.com'
-            description='I did this, that and the other'
-            version='v1.8'
-            status='completed'
-            technologies={technologies[2]}
-          /> */}
 
         </div>
 
