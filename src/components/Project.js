84915c989734765e274c957de8da33df8229abe9
brytorres <bryan@torvel.us>
2018-10-07 00:31:57 -0400

    Add projects

---
+++ src/components/Project.js
@@ -0,0 +1,60 @@
+import React, { Component } from 'react';
+
+import styles from '../styles/projects.module.scss';
+
+class Project extends Component {
+  // constructor(props) {
+  //   super(props);
+  // }
+
+  render() {
+
+    let image;
+    let badges;
+
+    if (this.props.image){
+      image = <div className={styles.image}>
+                <img src={this.props.image} alt="" />
+              </div>
+              
+    } else{
+      image = '';
+    }
+
+    if (this.props.version){
+      badges = <div className={styles.badges}>
+                <div className={styles.version}>{this.props.version}</div>
+                <div className={styles.status}>{this.props.status}</div>
+              </div>
+              
+    } else{
+      badges = '';
+    }
+
+    return (
+      <div className={styles.project}>
+
+        {image}
+
+        <a href={this.props.projectUrl}>
+          <h4 className={styles.client}>
+            {this.props.client}
+          </h4>
+        </a>
+
+        <p className={styles.title}>
+          {this.props.title}
+        </p>
+
+        <p className={styles.description}>
+          {this.props.description}
+        </p>
+
+        {badges}
+        
+      </div>
+    );
+  }
+}
+
+export default Project;
--- src/components/ProjectsSection.js
+++ src/components/ProjectsSection.js
@@ -1,12 +1,98 @@
 import React, { Component } from 'react'
+import Project from '../components/Project'
+// import config from '../config/config';
+
+import styles from '../styles/projects.module.scss';
 
 class ProjectsSection extends Component {
   render() {
+
+    const technologies = [
+      [
+        'HTML/CSS',
+        'Javascript'
+      ],
+      [
+        'HTML/CSS',
+        'Javascript',
+        'PHP (Laravel)'
+      ],
+      [
+        'HTML/CSS',
+        'Javascript',
+        'Python'
+      ]
+    ]
+
     return (
-      <div className="projects-section">
+      <div className={styles.projectsSection}>
+
         <h3>Client Projects</h3>
 
+        <div className={styles.projectsGroup}>
+
+          <Project
+            client = 'Retrograde Zone'
+            projectUrl = 'https://www.torvel.us'
+            description = 'I did this, that and the other'
+            image = 'urlHere'
+            status = 'broken'
+            technologies = {technologies[0]}
+          />
+
+          <Project
+            client = 'Mi Casa'
+            projectUrl = 'https://www.eatatmiscasa.com'
+            description = 'I did this, that and the other'
+            image = 'urlHere'
+            status = 'completed'
+            technologies = {technologies[1]}
+          />
+
+          <Project
+            client = 'Pro192 Studios'
+            projectUrl = 'https://www.pro192.com'
+            description = 'I did this, that and the other'
+            image = 'urlHere'
+            status = 'completed'
+            technologies = {technologies[2]}
+          />
+
+        </div>
+
         <h3>My Projects</h3>
+
+        <div className={styles.projectsGroup}>
+
+          <Project
+            client='Retrograde Zone'
+            projectUrl='https://www.torvel.us'
+            description='I did this, that and the other'
+            version='v2.1'
+            status='broken'
+            technologies={technologies[0]}
+          />
+
+          <Project
+            client='Mi Casa'
+            projectUrl='https://www.eatatmiscasa.com'
+            description='I did this, that and the other'
+            version='v2.0'
+            status='completed'
+            technologies={technologies[1]}
+          />
+
+          <Project
+            client='Pro192 Studios'
+            projectUrl='https://www.pro192.com'
+            description='I did this, that and the other'
+            version='v1.8'
+            status='completed'
+            technologies={technologies[2]}
+          />
+
+        </div>
+
       </div>
     );
   }
