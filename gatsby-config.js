dfb51e85f8532df0a3628a79fa42b22c27d09705
brytorres <bryan@torvel.us>
2018-10-07 22:47:46 -0400

    Clean config

--- gatsby-config.js
+++ gatsby-config.js
@@ -20,7 +20,6 @@ module.exports = {
         background_color: '#212121',
         theme_color: '#56a3a6',
         display: 'minimal-ui',
-        // icon: 'src/images/gatsby-icon.png',
       },
     },
     { 
@@ -30,6 +29,7 @@ module.exports = {
         accessToken: `f28089ba90b590a37c85ff02cccfc5420e3a78e01ed8b2d282e39168bc26862f`,
       },
     },
-    'gatsby-plugin-offline'
+    'gatsby-plugin-offline',
+    `gatsby-plugin-netlify`
   ],
 }
